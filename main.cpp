e0f8229779ca441085005fcf378f48d968154b1e
Wenzhe Dai <2628148d@student.gla.ac.uk>
2022-03-13 17:06:05 +0000

    engineering code

--- main.cpp
+++ main.cpp
@@ -1,116 +1,46 @@
 #include "pigpio.h"
+#include "motor_control.h"
+#include <iostream>
 using namespace std;
 //the GPIO is 6 13 19 26
 
-//set the every GPIO level
-void setStep(int h1, int h2, int h3, int h4)
+void set_mode(motor_control pi)
 {
-    gpioWrite(6, h1);
-    gpioWrite(13, h2);
-    gpioWrite(19, h3);
-    gpioWrite(26, h4);
+    pi.setMode(pi.IN1, 0);
+    pi.setMode(pi.IN2,0);
+    pi.setMode(pi.IN3,0);
+    pi.setMode(pi.IN4,0);
+
 }
-
-//pull up the high-level GPIO's resistance
-void pulldown(int h1, int h2, int h3, int h4)
-{
-    if(h1==1 && h2==0 && h3==0 && h4==0)
-    {
-        gpioSetPullUpDown(6, PI_PUD_UP);
-        gpioSetPullUpDown(13, PI_PUD_DOWN);
-        gpioSetPullUpDown(19, PI_PUD_DOWN);
-        gpioSetPullUpDown(26, PI_PUD_DOWN);
-    }
-
-    if(h1==0 && h2==1 && h3==0 && h4==0)
-    {
-        gpioSetPullUpDown(6, PI_PUD_DOWN);
-        gpioSetPullUpDown(6, PI_PUD_DOWN);
-        gpioSetPullUpDown(13, PI_PUD_UP);
-        gpioSetPullUpDown(19, PI_PUD_DOWN);
-        gpioSetPullUpDown(26, PI_PUD_DOWN);
-    }
-
-    if(h1==0 && h2==0 && h3==1 && h4==0)
-    {
-        gpioSetPullUpDown(6, PI_PUD_DOWN);
-        gpioSetPullUpDown(13, PI_PUD_DOWN);
-        gpioSetPullUpDown(19, PI_PUD_UP);
-        gpioSetPullUpDown(26, PI_PUD_DOWN);
-    }
-
-    if(h1==0 && h2==0 && h3==0 && h4==0)
-    {
-        gpioSetPullUpDown(6, PI_PUD_DOWN);
-        gpioSetPullUpDown(13, PI_PUD_DOWN);
-        gpioSetPullUpDown(19, PI_PUD_DOWN);
-        gpioSetPullUpDown(26, PI_PUD_UP);
-    }
-}
-
-//the number of loops is calculated by the angle
-int montor_angle(int angle)
-{
-    int j = (int)(angle/0.70312);
-    return j;
-}
-
 
 int main()
 {
-    if (gpioInitialise() < 0) return 1;  //init the librarys of pigpio
-
-    //set the type of GPIO(input or output)
-    gpioSetMode(6, PI_OUTPUT);
-    gpioSetMode(13, PI_OUTPUT);
-    gpioSetMode(19, PI_OUTPUT);
-    gpioSetMode(26, PI_OUTPUT);
-
-
-    int cir = montor_angle(3600); //turn it at 3600 degress
-    int i = 0;
-
-    //main loops
-    while (i<cir)
+    motor_control pi;
+    pi.IN1 = 6;
+    pi.IN2 = 13;
+    pi.IN3 = 19;
+    pi.IN4 = 26;
+    if (gpioInitialise()<0) return -1;    //init the librarys of pigpio
+    set_mode(pi);
+    int cir = pi.motor_angle(36000);
+    for(int i = 0;i<cir;i++)
     {
-        pulldown(1, 0, 0, 0);
-        setStep(1, 0, 0, 0);
+        pi.setPullUpDown(1, 0, 0, 0);
+        pi.setStep(1, 0, 0, 0);
         gpioSleep(PI_TIME_RELATIVE,0,2000);  //every impulse delay 2000ms
-        pulldown(0, 1, 0, 0);
-        setStep(0, 1, 0, 0);
+        pi.setPullUpDown(0, 1, 0, 0);
+        pi.setStep(0, 1, 0, 0);
         gpioSleep(PI_TIME_RELATIVE,0,2000);
-        pulldown(0, 0, 1, 0);
-        setStep(0, 0, 1, 0);
+        pi.setPullUpDown(0, 0, 1, 0);
+        pi.setStep(0, 0, 1, 0);
         gpioSleep(PI_TIME_RELATIVE,0,2000);
-        pulldown(0, 0, 0, 1);
-        setStep(0, 0, 0, 1);
+        pi.setPullUpDown(0, 0, 0, 1);
+        pi.setStep(0, 0, 0, 1);
         gpioSleep(PI_TIME_RELATIVE,0,2000);
-        i++;
     }
-
     gpioTerminate();  //stop the pigpio
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
