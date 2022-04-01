9989497c0b9df78c183a4ab96da58187a74d0c95
Wenzhe Dai <2628148d@student.gla.ac.uk>
2022-04-01 21:34:53 +0100

    rotation

--- main.cpp
+++ main.cpp
@@ -13,16 +13,8 @@ void set_mode(motor_control pi)
 
 }
 
-int main()
+void motor_spin(motor_control pi, int cir)
 {
-    motor_control pi;
-    pi.IN1 = 6;
-    pi.IN2 = 13;
-    pi.IN3 = 19;
-    pi.IN4 = 26;
-    if (gpioInitialise()<0) return -1;    //init the librarys of pigpio
-    set_mode(pi);
-    int cir = pi.motor_angle(36000);
     for(int i = 0;i<cir;i++)
     {
         pi.setPullUpDown(1, 0, 0, 0);
@@ -38,6 +30,45 @@ int main()
         pi.setStep(0, 0, 0, 1);
         gpioSleep(PI_TIME_RELATIVE,0,2000);
     }
+}
+
+void motor_reverse(motor_control pi, int cir)
+{
+    for(int i = 0;i<cir;i++)
+    {
+        pi.setPullUpDown(0, 0, 0, 1);
+        pi.setStep(0, 0, 0, 1);
+        gpioSleep(PI_TIME_RELATIVE,0,2000);  //every impulse delay 2000ms
+
+        pi.setPullUpDown(0, 0, 1, 0);
+        pi.setStep(0, 0, 1, 0);
+        gpioSleep(PI_TIME_RELATIVE,0,2000);
+
+        pi.setPullUpDown(0, 1, 0, 0);
+        pi.setStep(0, 1, 0, 0);
+        gpioSleep(PI_TIME_RELATIVE,0,2000);
+
+        pi.setPullUpDown(1, 0, 0, 0);
+        pi.setStep(1, 0, 0, 0);
+        gpioSleep(PI_TIME_RELATIVE,0,2000);
+    }
+
+}
+
+
+int main()
+{
+    motor_control pi;
+    pi.IN1 = 6;
+    pi.IN2 = 13;
+    pi.IN3 = 19;
+    pi.IN4 = 26;
+    if (gpioInitialise()<0) return -1;    //init the librarys of pigpio
+    set_mode(pi);
+    int cir = pi.motor_angle(180);
+    //motor_spin(pi, cir);
+    motor_reverse(pi, cir);
+
     gpioTerminate();  //stop the pigpio
     return 0;
 }
